2a4170599f8941c15f98fb206efa90c7b3d6bbbc
kiril <kirillmuhametkulov@gmail.com>
2025-01-29 23:40:15 +0300

    Created the Figure class. Added 4 new pictures

--- pygame_project.py
+++ pygame_project.py
@@ -7,7 +7,7 @@ from random import choice, randint
 
 FPS = 20
 WIDTH, HEIGHT = 600, 620
-BLOCK, CUP_H, CUP_W = 30, 20, 10
+BLOCK, CUP_H, CUP_W = 24, 20, 10
 FIGURE_W, FIGURE_H = 5, 5
 
 COLORS = ['red', 'blue', 'green', 'yellow']
@@ -89,14 +89,6 @@ def start_screen():
         clock.tick(FPS)
 
 
-def load_level(filename):
-    filename = filename
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-    max_width = max(map(len, level_map))
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-
-
 class Block(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y, color='empty'):
         super().__init__(tiles_group, all_sprites)
@@ -113,40 +105,44 @@ class FallenBlock(pygame.sprite.Sprite):
             10 + BLOCK * block_x, 10 + BLOCK * block_y)
 
 
+class Figure:
+    def __init__(self, shape, color, position):
+        self.shape, self.color, self.position = shape, color, position
+        self.x, self.y = int(CUP_W / 2) - int(FIGURE_W / 2), -2
+
+
 def generate_level(lev):
-    x, y = None, None
-    for y in range(len(lev)):
-        for x in range(len(lev[y])):
-            if lev[y][x] == 'o':
-                Block(y, x, 'empty')
-            elif lev[y][x][1] == 'b':
-                Block(y, x, lev[y][x][0])
-            elif lev[y][x][1] == 'f':
-                FallenBlock(y, x, lev[y][x][0])
-    return x, y
+    x1, y2 = None, None
+    for i in range(len(lev)):
+        for j in range(len(lev[i])):
+            if lev[i][j] == 'o':
+                Block(i, j, 'empty')
+            elif lev[i][j][1] == 'b':
+                Block(i, j, lev[i][j][0])
+            elif lev[i][j][1] == 'f':
+                FallenBlock(i, j, lev[i][j][0])
+    return x1, y2
 
 
 def createFigure(color):
-    fig = {'color': color,
-           'shape': (shape := choice(list(figures.keys()))),
-           'position': randint(0, len(figures[shape]) - 1),
-           'x': int(CUP_W / 2) - int(FIGURE_W / 2),
-           'y': -2}
+    shape = choice(list(figures.keys()))
+    position = randint(0, len(figures[shape]) - 1)
+    fig = Figure(shape, color, position)
     return fig
 
 
 def addToCup(fig):
     for i in range(FIGURE_W):
         for j in range(FIGURE_H):
-            if figures[fig['shape']][fig['position']][j][i] != 'o':
-                cup[i + fig['x']][j + fig['y']] = (fig['color'], 'f')
+            if figures[fig.shape][fig.position][j][i] != 'o':
+                cup[i + fig.x][j + fig.y] = (fig.color, 'f')
 
 
 def figureFalling(fig):
     for i in range(FIGURE_W):
         for j in range(FIGURE_H):
-            if figures[fig['shape']][fig['position']][j][i] != 'o':
-                Block(i + fig['x'], j + fig['y'], fig['color'])
+            if figures[fig.shape][fig.position][j][i] != 'o':
+                Block(i + fig.x, j + fig.y, fig.color)
 
 
 def speed_(p):
@@ -162,11 +158,11 @@ def figureInCup(fig_x, fig_y):
 def moveIsPossible(f, poss_x=0, poss_y=0):
     for i in range(FIGURE_W):
         for j in range(FIGURE_H):
-            if figures[f['shape']][f['position']][j][i] == 'o' or j + f['y'] + poss_y < 0:
+            if figures[f.shape][f.position][j][i] == 'o' or j + f.y + poss_y < 0:
                 continue
-            if not figureInCup(i + f['x'] + poss_x, j + f['y'] + poss_y):
+            if not figureInCup(i + f.x + poss_x, j + f.y + poss_y):
                 return False
-            if cup[i + f['x'] + poss_x][j + f['y'] + poss_y] != 'o':
+            if cup[i + f.x + poss_x][j + f.y + poss_y] != 'o':
                 return False
     return True
 
@@ -181,7 +177,7 @@ all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 
 tile_images = {
-    'empty': load_image('empty_square.png'),
+    'empty': load_image('blacksquare.png'),
     'red': load_image('red_square.png'),
     'blue': load_image('blue_square.png'),
     'green': load_image('green_square.png'),
@@ -197,6 +193,7 @@ fall = time.time()
 playing = True
 move_right = False
 move_left = False
+move_down = False
 points = 0
 level, fall_speed = 1, 0.25
 
@@ -213,17 +210,24 @@ while playing:
             terminate()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT and moveIsPossible(figure, poss_x=-1):
-                figure['x'] -= 1
+                figure.x -= 1
                 move_right = False
                 move_left = True
+                move_down = False
             elif event.key == pygame.K_RIGHT and moveIsPossible(figure, poss_x=1):
                 move_right = True
                 move_left = False
-                figure['x'] += 1
+                move_down = False
+                figure.x += 1
+            elif event.key == pygame.K_DOWN and moveIsPossible(figure, poss_y=1):
+                move_down = True
+                move_right = False
+                move_left = False
+                figure.y += 1
             elif event.key == pygame.K_UP:
-                figure['position'] = (figure['position'] + 1) % len(figures[figure['shape']])
+                figure.position = (figure.position + 1) % len(figures[figure.shape])
                 if not moveIsPossible(figure):
-                    figure['position'] = (figure['position'] - 1) % len(figures[figure['shape']])
+                    figure.position = (figure.position - 1) % len(figures[figure.shape])
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_SPACE:
@@ -232,19 +236,24 @@ while playing:
                 move_left = False
             elif event.key == pygame.K_RIGHT:
                 move_right = False
+            elif event.key == pygame.K_DOWN:
+                move_down = False
 
     if time.time() - fall > fall_speed:
         if not moveIsPossible(figure, poss_y=1):
             addToCup(figure)
             figure = None
         else:
-            figure['y'] += 1
+            figure.y += 1
             fall = time.time()
-    screen.fill('white')
+    screen.fill(pygame.Color(150, 150, 150))
     for i in all_sprites:
         i.kill()
     generate_level(cup)
     if not figure is None:
-        figureFalling(figure)
+        if figure.y != -2:
+            figureFalling(figure)
+    pygame.draw.rect(screen, 'black', (10, 10, 246, 486))
     tiles_group.draw(screen)
+    pygame.draw.rect(screen, 'white', (10, 10, 246, 486), 3)
     pygame.display.flip()
